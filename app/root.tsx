f0bb8065bd70aeb35e52cbfa8028b32c7d87788b
Adryan Frey <adryanfrey886@gmail.com>
2025-09-14 09:33:02 +0200

    crud dashboard temp;ate

--- app/root.tsx
+++ app/root.tsx
@@ -30,13 +30,13 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <CssBaseline />
         <Meta />
         <Links />
       </head>
       <body>
         {children}
         <ScrollRestoration />
-        <CssBaseline />
         <Scripts />
       </body>
     </html>
